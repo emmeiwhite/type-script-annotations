d65cf079f9b1c2a22be055ee9cd34eb43e25486d
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-07 20:08:24 +0530

    Type Annotations with primitive types and build-in objects

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,1 +1,14 @@
-const apples: number = 5;
+let apples: number = 5;
+
+let speed: string = "fast";
+
+let isRaining: boolen = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+// build in objects
+let date: Date = new Date();
+
+/** Type Annotations are used with any type of value - literally any type of value */
